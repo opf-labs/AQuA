b473cceb06fabfaa0e31621b1a89c068855a946e
techmaurice <maurice.de.rooij@nationaalarchief.nl>
2011-08-24 11:06:58 +0200

    Added Linux shell script for recursive processing

---
+++ office-analyser/jar/office_analyser_folder.sh
@@ -0,0 +1,18 @@
+# /******************************************************************************************/
+# AQuA Office Analyser recursive folder batch for Windows
+# License: APACHE 2.0
+# Author: Maurice de Rooij (OPF/NANETH) <maurice.de.rooij@nationaalarchief.nl>, August 2011
+# /******************************************************************************************/
+# USAGE: office_analyser_folder.sh "/path/to/office/files"
+# OUTPUT: filename.doc.analysis.txt with analysis results is written next to every analysed file
+# ERRORS: error messages are also written to this analysis file
+# /******************************************************************************************/
+echo AQuA Office Analyser started
+for f in $(find $1 -iname "*.doc")
+do
+if [ -a $f ]; then
+echo Analysing file: $f
+java -jar office_analyser.jar $f >$f.analyse.txt 2>&1
+fi
+done
+echo AQuA Office Analyser finished
